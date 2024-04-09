7e003c661acba03f4fe2ae50a3caec9988b3943e
Douglas Isturiz <disturiz362@gmail.com>
2024-04-09 16:13:53 -0600

    Manejo de rutas

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, url_for
+from werkzeug.exceptions import abort
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def inicio():
@@ -8,3 +11,31 @@ def inicio():
     app.logger.warning('Mensaje a nivel Warning')
     app.logger.error('Mensaje a nivel Error')
     return 'Hola, Mundo desde Flask!'
+
+
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    return f'Saludos soy {nombre}'
+
+
+@app.route('/edad/<int:edad>')
+def mostrar_edad(edad):
+    return f'Saludos tengo {edad} años'
+
+
+@app.route('/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_nombre(nombre):
+    return render_template('mostrar.html', nombre=nombre)
+
+
+@app.route('/redireccionar')
+def redireccionar():
+    return redirect(url_for('inicio'))
+
+@app.route('/salir')
+def salir():
+    return abort(404)
+
+@app.errorhandler(404)
+def pagina_no_encontrada(error):
+    return render_template('error404.html', error=error), 404
