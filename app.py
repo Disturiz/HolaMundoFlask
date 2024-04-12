55f53609d5d0f1dc06f20c4dacbc549ff4502b88
Douglas Isturiz <disturiz362@gmail.com>
2024-04-12 15:16:12 -0600

    Manejo de sesiones

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, jsonify, session
 from werkzeug.exceptions import abort
 from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
+app.secret_key = 'Mi_llave_secreta'
+
 
 @app.route('/')
 def inicio():
-    app.logger.info(f'Entramos al Path {request.path}')
-    app.logger.warning('Mensaje a nivel Warning')
-    app.logger.error('Mensaje a nivel Error')
-    return 'Hola, Mundo desde Flask!'
+    if 'username' in session:
+        return f'El usuario ya ha hecho Login {session["username"]}'
+    return 'No ha hecho Login!'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Omitimos validación del usuario
+        usuario = request.form['username']
+        # Agregar el usuario a la sesión
+        # session['username'] = usuario
+        session['username'] = request.form['username']
+        return redirect(url_for('inicio'))
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect(url_for('inicio'))
 
 
 @app.route('/saludar/<nombre>')
@@ -32,10 +51,19 @@ def mostrar_nombre(nombre):
 def redireccionar():
     return redirect(url_for('inicio'))
 
+
 @app.route('/salir')
 def salir():
     return abort(404)
 
+
 @app.errorhandler(404)
 def pagina_no_encontrada(error):
     return render_template('error404.html', error=error), 404
+
+
+# REST Representational State Transfer
+@app.route('/api/mostrar/<nombre>', methods=['GET', 'POST'])
+def mostrar_json(nombre):
+    valores = {'nombre': nombre, 'metodo_http': request.method}
+    return jsonify(valores)
